5e908b40af256b9da01d9fc00e0778e79818837e
GuppyCoder <102766209+GuppyCoder@users.noreply.github.com>
2023-05-19 08:10:10 -0500

    Handle edge cases
    ToTheBack.cpp did not handle certain scenarios. The program now makes
    sure to never have a negative length, return length 1 where it instead returned 0, etc.

--- Arrays101/ToTheBack.cpp
+++ Arrays101/ToTheBack.cpp
@@ -3,11 +3,11 @@
 // Goal: Push all the Ones to the back of the array
 int main()
 {
-    int array[2] = {3, 1};
+    int array[2] = {3, 3};
     int length = sizeof(array)/ sizeof(int);
     for(int i = 0; i < length; i++)
     {
-        while(array[i] == 3)
+        while(array[i] == 3 && i < length && length > 0)
         {
             array[i] = array[length - 1];
             length--;
